193638f39e230e2b09ac0190f78fcba980608ce5
Pegasus Wang <tianma201211@gmail.com>
2016-03-01 07:51:09 +0800

    modify post get post from mongodb

--- handlers/post.py
+++ handlers/post.py
@@ -2,14 +2,32 @@
 # -*- coding:utf-8 -*-
 
 
+import _env
 from .base import BaseHandler
+from lib.date_util import datestr_from_stamp
+from lib.json_tools import bson_to_json
+from tornado.gen import coroutine
 from tornado.web import url
+from tornado.util import ObjectDict
 
 
 class IndexHandler(BaseHandler):
-    def get(self):
-        self.render('index.html')
-        #self.render('default.html')
+    @property
+    def _db(self):
+        return self.application._motor.wechat_post
+
+    @coroutine
+    def get(self, page=1):
+        posts = []
+        pre_url = 'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl='
+        cursor = self._db.find().sort('ori_create_time', -1)
+        for doc in (yield cursor.to_list(length=18)):
+            post = bson_to_json(doc)
+            post['image'] = pre_url+post['cdn_url']
+            post['date'] = datestr_from_stamp(post['ori_create_time'],
+                                              '%Y-%m-%d')
+            posts.append(ObjectDict(post))
+        self.render('index.html', posts=posts)
 
 
 URL_ROUTES = [
---
+++ lib/date_util.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import datetime
+
+
+def datestr_from_stamp(time_stamp, format_str):
+    """strftime('%Y-%m-%d %H:%M:%S')"""
+    try:
+        return (
+            datetime.datetime.fromtimestamp(
+                int(time_stamp)
+            ).strftime(format_str)
+        )
+    except ValueError:
+        return (
+            datetime.datetime.fromtimestamp(
+                int(time_stamp/1000.0)
+            ).strftime(format_str)
+        )
+
+
+def datetime_from_stamp(time_stamp):
+    try:
+        return (
+            datetime.datetime.fromtimestamp(
+                int(time_stamp)
+            )
+        )
+    except ValueError:
+        return (
+            datetime.datetime.fromtimestamp(
+                int(time_stamp/1000.0)
+            )
+        )
+
+
+def days_from_epoch(year, month, day):
+    """从1970.01.01到给定日期的天数"""
+    date_time = datetime.datetime(year, month, day)
+    beg_date_time = datetime.datetime(1970, 1, 1)
+    return (date_time-beg_date_time).days
+
+
+def datetime_from_days(days):
+    """从1970.01.01的天数到datetime对象"""
+    beg_date_time = datetime.datetime(1970, 1, 1)
+    return beg_date_time + datetime.timedelta(days=days)
+
+
+def days_from_slash_date(date_str):
+    '''1990/08/22'''
+    if date_str.count('/') == 1:
+        year, month = date_str.split('/')
+        if len(year) < len(month):
+            year, month = month, year
+        return days_from_epoch(int(year), int(month), day=1)
+
+    elif date_str.count('/') == 2:
+        year, month, day = map(int, date_str.split('/'))
+        return days_from_epoch(year, month, day)
+    else:
+        print(date_str)
+        return date_str
+
+
+if __name__ == '__main__':
+    print(datetime_from_stamp(757382400000))
+    print(datestr_from_stamp(757382400000))
+    print(type(datetime_from_stamp(757382400000)))
+    print(days_from_slash_date('2000/08/24'))
+    print(days_from_slash_date('1990/03/29'))
+    print(days_from_slash_date('12/1989'))
+    print(days_from_slash_date('2000/08/24'))
+    print(type(days_from_slash_date('2000/08/24')))
+    print(datetime_from_days(days_from_epoch(2015,1,1)))
