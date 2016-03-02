f63908a0f182c9d3507e1e92e544e68a6de574f3
Pegasus Wang <tianma201211@gmail.com>
2016-03-02 18:17:06 +0800

    add pagination

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import tornado.gen
-from tornado.options import options, define
+from tornado.options import options
 from urls import url_patterns
 from motorengine.connection import connect
 from settings import settings, TEMPLATE_PATH
 
 
-class ArticlesApp(tornado.web.Application):
+class App(tornado.web.Application):
     def __init__(self):
         tornado.web.Application.__init__(self, url_patterns, **settings)
         self._redis = _db.redis_client
@@ -27,11 +27,12 @@ class ArticlesApp(tornado.web.Application):
 
 def main():
     tornado.options.parse_command_line()
-    app = ArticlesApp()
+    app = App()
     http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
 
     if options.debug:
+        # autorelaod for template file
         tornado.autoreload.start()
         for root, dir, files in os.walk(TEMPLATE_PATH):
             for item in files:
--- handlers/post.py
+++ handlers/post.py
@@ -6,9 +6,12 @@ import _env
 from .base import BaseHandler
 from lib.date_util import datestr_from_stamp
 from lib.json_tools import bson_to_json
+from config.config import CONFIG
 from tornado.gen import coroutine
 from tornado.web import url
 from tornado.util import ObjectDict
+
+SITE = CONFIG.SITE
 
 
 class IndexHandler(BaseHandler):
@@ -18,10 +21,12 @@ class IndexHandler(BaseHandler):
 
     @coroutine
     def get(self, page=1):
+        request_url = self.request.full_url()
+        print(request_url)
         posts = []
         pre_url = 'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl='
         cursor = self._db.find().sort('ori_create_time', -1)
-        for doc in (yield cursor.to_list(length=18)):
+        for doc in (yield cursor.to_list(length=SITE.POSTS_PER_PAGE)):
             post = bson_to_json(doc)
             post['image'] = pre_url+post['cdn_url']
             post['date'] = datestr_from_stamp(post['ori_create_time'],
